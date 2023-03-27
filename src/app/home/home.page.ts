56f7c396e72953f5eee66d2df92b3dd538d6382f
Brayan Decanini <gominola3008@gmail.com>
2023-03-26 23:25:18 -0700

    Rating Stars

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,46 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  stars: number[] = [1, 2, 3, 4, 5];
+
+  selectedValue: number = 0;
+  isMouseover = true;
+
+  public data: any;
+
+  constructor() { }
+
+  ngOnInit() {
+    console.log(this.stars);
+    console.log(this.selectedValue);
+  }
+
+  //counting stars
+
+  countStar(star: number) {
+    this.isMouseover = false;
+    this.selectedValue = star;
+    this.data = this.selectedValue;
+    console.log(this.data);
+  }
+
+  //for adding star
+
+  addClass(star: number) {
+    if (this.isMouseover) {
+      this.selectedValue = star;
+      console.log(this.selectedValue);
+
+    }
+  }
+
+  //for removing star
+
+  removeClass() {
+    if (this.isMouseover) {
+      this.selectedValue = 0;
+      console.log(this.selectedValue);
+    }
+  }
 
 }
